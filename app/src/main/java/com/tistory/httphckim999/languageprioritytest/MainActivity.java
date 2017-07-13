2a9c6498b09d3d448b4d60084203e93017667712
Hyunchul Kim <kimhc999@gmail.com>
2017-07-13 20:26:54 +0900

    Initial commit

---
+++ app/src/main/java/com/tistory/httphckim999/languageprioritytest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tistory.httphckim999.languageprioritytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
